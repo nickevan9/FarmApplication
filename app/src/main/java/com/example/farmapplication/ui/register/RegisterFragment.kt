0f7fbc18afd51f32646e16363257de17a5864ac5
evan nick <nickevan9@gmail.com>
2020-05-14 00:23:34 +0700

    avc

--- app/src/main/java/com/example/farmapplication/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/farmapplication/ui/register/RegisterFragment.kt
@@ -1,6 +1,5 @@
 package com.example.farmapplication.ui.register
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
